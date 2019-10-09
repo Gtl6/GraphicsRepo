422a90559f60a86d2f6139c3d2b2d1b1625db3e4
Griffin <gtl6@pitt.edu>
2019-10-08 22:34:11 -0400

    Completely defined the Spring... it probably won't compile, but I'm pretty sure all the fancy shit is right. Just need to get it plugged in.

--- Proj1/lab4.c
+++ Proj1/lab4.c
@@ -23,38 +23,135 @@
 #include "matrixlib.h"
 
 #define BUFFER_OFFSET( offset )   ((GLvoid*) (offset))
-
-vec4 vertices[6] =
-{{ 0.0,  0.5,  0.0, 1.0},	// top
- {-0.5, -0.5,  0.0, 1.0},	// bottom left
- { 0.5, -0.5,  0.0, 1.0},	// bottom right
- { 0.5,  0.8, -0.5, 1.0},	// top
- { 0.9,  0.0, -0.5, 1.0},	// bottom right
- { 0.1,  0.0, -0.5, 1.0}};	// bottom left
-
-vec4 colors[6] =
-{{1.0, 0.0, 0.0, 1.0},	// red   (for top)
- {0.0, 1.0, 0.0, 1.0},	// green (for bottom left)
- {0.0, 0.0, 1.0, 1.0},	// blue  (for bottom right)
- {0.0, 1.0, 0.0, 1.0},	// green (for bottom right)
- {0.0, 1.0, 0.0, 1.0},	// green (for bottom right)
- {0.0, 1.0, 0.0, 1.0}};	// green (for bottom right)
+#define PI 3.14159265
 
 const float circle_radius = 0.05;
 const int circle_resolution = 8;
 const float coil_space = 0.05;
 const int num_coils = 5;
 const int coil_resolution = 8;
+const int coil_center_radius = 0.2;
 
 // Calculates the number of vertices
-int NUMVERTICES = 6 * circle_resolution * coil_resolution * num_coils + 6 * circle_resolution;
+int CIRCLEVERTCOUNT = 3 * circle_resolution;
+int COILVERTCOUNT = 6 * circle_resolution * coil_resolution;
+int NUMVERTICES = COILVERTCOUNT * num_coils + 2 * CIRCLEVERTCOUNT;
+float CIRCANGLE = 2 * PI / circle_radius;
+float TUBEANGLE = 2 * PI / coil_resolution;
+float TUBEHEIGHTS = (2 * circle_radius + coil_space) / coil_resolution;
 
 GLuint ctm_location;
 mat4 ctm;
 
+// Will generate just one circle at a time, because I like making functions and offloading effort instead of actually writing code
+void generate_one_circle(vec4 *arr, float x, float y, float z, char is_left){
+	int i;
+	for(i = 0; i < CIRCLEVERTCOUNT; i++){
+		// Dealing with the angle_numth triangle
+		int angle_num = i / 3;
+		float theta = CIRCANGLE * angle_num;
+
+		vec4 center = {x, y, z, 1.0};
+		vec4 p1 = {x, y + circle_radius * sin(theta), z + circle_radius * cos(theta), 1.0};
+		vec4 p2 = {x, y + circle_radius * sin(theta + CIRCANGLE), z + circle_radius * cos(theta + CIRCANGLE), 1.0};
+
+		// Load the vertices into the array
+		arr[i] = center;
+		i++;
+
+		if(is_left){
+			arr[i] = p1;
+			i++;
+			arr[i] = p2;
+		}
+		else{
+			arr[i] = p2;
+			i++;
+			arr[i] = p1;
+		}
+	}
+}
+
+// Will generate the circles on the ends of the spring
+void generate_circles(vec4 *arr){
+	float x = 0;
+	float y = 1.0 / 2.0 * (num_coils) * (coil_space + 2 * circle_radius); // We have our spring centered at the origin, so this is gross
+	float z = inner_radius + circle_radius;
+
+	generate_one_circle(arr, x, y, z, 0);
+	generate_one_circle(arr + CIRCLEVERTCOUNT, x, -1 * y, z, 1) // But since it's at the origin, this is easy
+}
+
+float top_radius(float front_angle){
+	return coil_center_radius + circle_radius * cos(front_angle);
+}
+
+// Creates a "tube", or a single chunk of a taurus
+void generate_tube(vec4 *arr, float height, int which_tube){
+	int i;
+	float top_angle1 = which_tube * TUBEANGLE;
+	float top_angle2 = (which_tube + 1) * TUBEANGLE;
+	float height2 = height + TUBEHEIGHTS;
+
+	for(i = 0; i < 6 * circle_resolution; i++){
+		int angle_count = i / 6;
+		float front_angle1 = angle_count * CIRCLEANGLE;
+		float front_angle2 = (angle_count + 1) * CIRCLEANGLE;
+		// In order, our point, the next point on the circle, our point on the other side of the tube, and the next point on the other side of the tube
+		// Do you ever look at a line of code (or four) and wonder where your life went so horribly wrong?
+		vec4 p1 = {top_radius(front_angle1) * sin(top_angle1), height + sin(front_angle1), top_radius(front_angle1) * cos(top_angle1), 1.0};
+		vec4 p2 = {top_radius(front_angle2) * sin(top_angle1), height + sin(front_angle2), top_radius(front_angle2) * cos(top_angle1), 1.0};
+		vec4 p3 = {top_radius(front_angle1) * sin(top_angle2), height2 + sin(front_angle1), top_radius(front_angle1) * cos(top_angle2), 1.0};
+		vec4 p4 = {top_radius(front_angle2) * sin(top_angle2), height2 + sin(front_angle2), top_radius(front_angle2) * cos(top_angle2), 1.0};
+
+		// On the plus side, I somehow managed to make it so that the surface's normal angle doesn't matter
+		// Don't ask me how. Absolutely bonkers how that worked out.
+		arr[i] = p1;
+		i++;
+		arr[i] = p3;
+		i++;
+		arr[i] = p4;
+		i++;
+		arr[i] = p1;
+		i++;
+		arr[i] = p4;
+		i++;
+		arr[i] = p2;
+	}
+}
+
+void generate_coil(vec4 *arr, float height){
+	int i;
+	int VERTSPERTUBE = 6 * circle_resolution;
+
+	for(i = 0; i < coil_resolution; i++){
+		generate_tube(arr + i * VERTSPERTUBE, height + i * TUBEHEIGHTS, i);
+	}
+}
+
 // The spring is generated depending on input variables as specified above
 void generate_spring(vec4 *arr){
-	// My Code
+	generate_circles(arr);
+	float start_height = 0;
+	float coil_height = 2 * circle_radius + coil_space;
+
+	// If we have an odd number of coils, the middle will be the middle of a coil
+	// With an even number, we have to shift it up by half a coil
+	if(num_coils % 2 == 0){
+		start_height = coil_height / 2;
+	}
+
+	// Note num_coils results in int division. This is on purpose (so 5 drops two coils, as would 4, so that's why we need the shifting)
+	start_height -= (num_coils / 2) * coil_height;
+
+	// To account for the fact we've already populated the array with two circles
+	int circle_offset = 2 * CIRCLEVERTCOUNT;
+
+	int i;
+	for(i = 0; i < num_coils; i++){
+		// Build a coil in the array (at the right spot) at the height given
+		generate_coil(arr + circle_offset + i * (COILVERTCOUNT), start_height + i * coil_height);
+	}
 }
 
 float random_col(){
